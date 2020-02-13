5dc668b33c7a03dd2e2c1574d24879630a5098ed
VDS13 <mit_vjazin@mail.ru>
2020-02-13 11:42:28 +0300

    Добавлен ивент на нажатие enter в комментариях

--- js/comments.js
+++ js/comments.js
@@ -32,6 +32,38 @@ function comment_and_like() {
         }
     };
 }
+document.getElementById('comments').addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) {
+        var xhr = new XMLHttpRequest();
+        var text = document.getElementById('comments').value;
+        var omg = encodeURIComponent(text);
+        var body = "msg=" + omg;
+        xhr.open("POST", "write.php");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.setRequestHeader("Content-Length", body.length);
+        xhr.setRequestHeader("Connection", "close");
+        xhr.send(body);
+        document.getElementById('comments').value = "";
+        xhr.onload = function() {
+            if (xhr.status != 200) { 
+                alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+            } else {
+                var xhr1 = new XMLHttpRequest();
+                xhr1.open("GET", "comments.php");
+                xhr1.send();
+                xhr1.onload = function() {
+                    if (xhr1.status != 200) { 
+                        alert(`Ошибка ${xhr1.status}: ${xhr1.statusText}`);
+                    } else {
+                        var z = document.getElementById("chati");
+                        z.innerHTML = xhr1.response;
+                        z.scrollTop = 9999;
+                    }
+                };
+            }
+        };
+    }
+  });
 document.getElementById('but').onclick = function send() {
     var xhr = new XMLHttpRequest();
     var text = document.getElementById('comments').value;
